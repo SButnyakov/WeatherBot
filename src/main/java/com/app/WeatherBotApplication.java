270a40468e94fee0549325cdc80f25a51c65bb79
Vlad <vladchukaev@gamil.com>
2022-02-09 17:52:31 +0300

    Отмена Накормки
    Не покушоли(

--- src/main/java/com/app/WeatherBotApplication.java
+++ src/main/java/com/app/WeatherBotApplication.java
@@ -3,6 +3,5 @@ package com.app;
 public class WeatherBotApplication {
     public static void main(String[] args) {
         Bot WeatherBot = new Bot("BOT_NAME", "BOT_TOKEN");
-        System.out.println("База. Накормила");
     }
 }
