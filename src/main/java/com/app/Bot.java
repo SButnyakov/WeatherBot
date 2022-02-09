13f37cc366f0baa7cfc2479b027df4e45d7b4b6e
Sergey <sergey.butnyakov@gmail.com>
2022-02-09 17:36:18 +0300

    Base Bot commit
    База. Кормит. Проверяйте.
    
    Co-Authored-By: chukaevVS <90568706+chukaevVS@users.noreply.github.com>

---
+++ src/main/java/com/app/Bot.java
@@ -0,0 +1,30 @@
+package com.app;
+
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Bot extends TelegramLongPollingCommandBot {
+    private final String BOT_NAME;
+    private final String BOT_TOKEN;
+
+    public Bot(String botName, String botToken) {
+        super();
+        this.BOT_NAME = botName;
+        this.BOT_TOKEN = botToken;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void processNonCommandUpdate(Update update) {
+
+    }
+}
---
+++ src/main/java/com/app/WeatherBotApplication.java
@@ -0,0 +1,7 @@
+package com.app;
+
+public class WeatherBotApplication {
+    public static void main(String[] args) {
+        Bot WeatherBot = new Bot("BOT_NAME", "BOT_TOKEN");
+    }
+}
