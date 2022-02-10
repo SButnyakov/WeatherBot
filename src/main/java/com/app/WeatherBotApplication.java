7da715f7ee4a28a3fcf8a46afca4d46222c7a05a
Sergey <sergey.butnyakov@gmail.com>
2022-02-10 03:31:06 +0300

    Исправил опечатки и плюсанул версию

--- src/main/java/com/app/WeatherBotApplication.java
+++ src/main/java/com/app/WeatherBotApplication.java
@@ -8,10 +8,10 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class WeatherBotApplication {
     private static final String BOT_NAME = "WeatherBot";
     private static final String BOT_TOKEN = "5171636256:AAGnR8-v0srYgX4tsvG1sYlt7IoGgP4Dz1E";
-    public static void main(String[] args) {\
+    public static void main(String[] args) {
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(new Bot(BOT_NAME, BOT_TOKEN))
+            telegramBotsApi.registerBot(new Bot(BOT_NAME, BOT_TOKEN));
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
