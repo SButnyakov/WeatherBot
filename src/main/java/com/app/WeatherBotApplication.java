78dd6510162632e369ddba8fedf3a5964d7a6c8c
Sergey <sergey.butnyakov@gmail.com>
2022-02-10 05:26:22 +0300

    5:26 время...

---
+++ src/main/java/com/app/WeatherBotApplication.java
@@ -0,0 +1,20 @@
+package com.app;
+
+import com.app.bot.Bot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class WeatherBotApplication {
+    private static final String BOT_NAME = "WeatherBot";
+    private static final String BOT_TOKEN = "5171636256:AAGnR8-v0srYgX4tsvG1sYlt7IoGgP4Dz1E";
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Bot(BOT_NAME, BOT_TOKEN));
+            System.out.println("hello world");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
