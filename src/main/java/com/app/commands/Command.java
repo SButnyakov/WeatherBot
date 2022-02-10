5323e148eb66028c868ec4afb0d72c699cc3d552
Sergey <sergey.butnyakov@gmail.com>
2022-02-10 17:06:45 +0300

    Update Command.java

--- src/main/java/com/app/commands/Command.java
+++ src/main/java/com/app/commands/Command.java
@@ -8,7 +8,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public abstract class Command extends BotCommand {
     public static final String API_TOKEN = "db082d6b19ebbda556c7e2e01d9b36b5";
-    final OpenWeatherMapClient openWeatherClient = new OpenWeatherMapClient(API_TOKEN);
+    public final OpenWeatherMapClient openWeatherClient = new OpenWeatherMapClient(API_TOKEN);
 
     public Command(String commandIdentifier, String description) {
         super(commandIdentifier, description);
