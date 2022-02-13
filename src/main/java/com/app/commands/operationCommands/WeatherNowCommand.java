b655635c87cc08d99612dcdb8f24ec498636ca63
Sergey <sergey.butnyakov@gmail.com>
2022-02-13 14:45:17 +0300

    Добавлено давление

--- src/main/java/com/app/commands/operationCommands/WeatherNowCommand.java
+++ src/main/java/com/app/commands/operationCommands/WeatherNowCommand.java
@@ -9,6 +9,7 @@ import com.github.prominence.openweathermap.api.enums.UnitSystem;
 import com.github.prominence.openweathermap.api.model.weather.Rain;
 import com.github.prominence.openweathermap.api.model.weather.Snow;
 import com.github.prominence.openweathermap.api.model.weather.Weather;
+import com.github.prominence.openweathermap.api.model.weather.Wind;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
@@ -49,6 +50,8 @@ public class WeatherNowCommand extends Command {
                 "\n" +
                 getWindLine(weather) +
                 "\n" +
+                getHPaPressureLine(weather) +
+                "\n" +
                 getHumidityLine(weather);
     }
 
@@ -68,16 +71,15 @@ public class WeatherNowCommand extends Command {
         temperature = (temp >= 30) ? Emojis.THIRTY_AND_HIGHER + " " + temp : temperature;
         temperature = (temp >= 20 && temp < 30) ? Emojis.TWENTY_AND_TWENTY_NINE + " " + temp : temperature;
         temperature = (temp >= 10 && temp < 20) ? Emojis.TEN_AND_NINETEEN + " " + temp : temperature;
-        temperature = (temp >= 0 && temp < 10) ? Emojis.ZERO_AND_NINE + " " + temp : temperature;
-        temperature = (temp >= -15 && temp < 0) ? Emojis.SUB_FIFTEEN_AND_ONE + " " + temp : temperature;
+        temperature = (temp >= 1 && temp < 10) ? Emojis.ZERO_AND_NINE + " " + temp : temperature;
+        temperature = (temp >= -15 && temp < 1) ? Emojis.SUB_FIFTEEN_AND_ONE + " " + temp : temperature;
         temperature = (temp >= -20 && temp < -15) ? Emojis.SUB_TWENTY_AND_SIXTEEN + " " + temp : temperature;
         temperature = (temp < -20) ? Emojis.SUB_TWENTY_ONE + " " + temp : temperature;
         return temperature + " °C\n";
     }
 
     private String getHumidityLine(Weather weather) {
-        int humidity = weather.getHumidity().getValue();
-        return Emojis.HUMIDITY + " " + humidity + "%";
+        return Emojis.HUMIDITY + " " + weather.getHumidity().getValue() + "%";
     }
 
     private String getCloudinessLine(Weather weather) {
@@ -99,26 +101,27 @@ public class WeatherNowCommand extends Command {
         } else if (rainTemp != null) {
             double rain = rainTemp.getOneHourLevel();
             String rainLine = "";
-            rainLine += (rain < 4.0) ? Emojis.SMALL_RAIN_1_2 + " Небольшой дождь" : rainLine;
-            rainLine += (rain >= 4.0 && rain < 15.0) ? Emojis.SMALL_RAIN_1_2 + " Дождь" : rainLine;
-            rainLine += (rain >= 15.0 && rain < 50.0) ? Emojis.RAIN_1_2 + " Сильный дождь" : rainLine;
-            rainLine += (rain >= 50.0) ? Emojis.RAIN_1_2 + " Очень сильный дождь" : rainLine;
+            rainLine = (rain < 4.0) ? Emojis.SMALL_RAIN_1_2 + " Небольшой дождь" : rainLine;
+            rainLine = (rain >= 4.0 && rain < 15.0) ? Emojis.SMALL_RAIN_1_2 + " Дождь" : rainLine;
+            rainLine = (rain >= 15.0 && rain < 50.0) ? Emojis.RAIN_1_2 + " Сильный дождь" : rainLine;
+            rainLine = (rain >= 50.0) ? Emojis.RAIN_1_2 + " Очень сильный дождь" : rainLine;
             return rainLine + "\n";
         } else if (snowTemp != null) {
             double snow = snowTemp.getOneHourLevel();
             String snowLine = "";
-            snowLine += (snow < 4.0) ? Emojis.SNOW_1_2_3_4 + " Небольшой снег" : snowLine;
-            snowLine += (snow >= 4.0 && snow < 15.0) ? Emojis.SNOW_1_2_3_4 + " Снег" : snowLine;
-            snowLine += (snow >= 15.0 && snow < 50.0) ? Emojis.SNOW_1_2_3_4 + " Сильный снег" : snowLine;
-            snowLine += (snow >= 50.0) ? Emojis.SNOW_1_2_3_4 + " Очень сильный снег" : snowLine;
+            snowLine = (snow < 4.0) ? Emojis.SNOW_1_2_3_4 + " Небольшой снег" : snowLine;
+            snowLine = (snow >= 4.0 && snow < 15.0) ? Emojis.SNOW_1_2_3_4 + " Снег" : snowLine;
+            snowLine = (snow >= 15.0 && snow < 50.0) ? Emojis.SNOW_1_2_3_4 + " Сильный снег" : snowLine;
+            snowLine = (snow >= 50.0) ? Emojis.SNOW_1_2_3_4 + " Очень сильный снег" : snowLine;
             return snowLine + "\n";
         }
         return Emojis.NO_PRECIPITATION + " Без осадков!\n";
     }
 
     private String getWindLine(Weather weather) {
-        try {
-            byte wind = (byte) Math.round(weather.getWind().getSpeed());
+        Wind windTemp = weather.getWind();
+        if (windTemp != null) {
+            byte wind = (byte) Math.round(windTemp.getSpeed());
             String windLine = "";
             windLine = (wind <= 5) ? Emojis.WIND_1 + " Слабый ветер" : windLine;
             windLine = (wind > 5 && wind <= 14) ? Emojis.WIND_2 + " Умеренный ветер" : windLine;
@@ -126,9 +129,11 @@ public class WeatherNowCommand extends Command {
             windLine = (wind > 24 && wind <= 32) ? Emojis.WIND_3 + " Очень сильный ветер" : windLine;
             windLine = (wind > 32) ? Emojis.WIND_4 + " Ураганный ветер" : windLine;
             return windLine + "\n";
-        } catch (NullPointerException e) {
-            e.printStackTrace();
         }
-        return "Ветер умер";
+        return Emojis.WIND_1 + " Штиль\n";
+    }
+
+    private String getHPaPressureLine(Weather weather) {
+        return Emojis.PRESSURE + " " + Math.round(weather.getAtmosphericPressure().getValue()) + " hPa\n";
     }
 }
