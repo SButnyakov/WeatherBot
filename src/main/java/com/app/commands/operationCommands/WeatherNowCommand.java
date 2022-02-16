5c0113e5ebaa3d156cd620eeb403cba7c9cd4ec1
Vlad <vladchukaev@gamil.com>
2022-02-16 18:31:05 +0300

    Добавил в парсер обработку дождя вместе со снегом

--- src/main/java/com/app/commands/operationCommands/WeatherNowCommand.java
+++ src/main/java/com/app/commands/operationCommands/WeatherNowCommand.java
@@ -2,18 +2,14 @@ package com.app.commands.operationCommands;
 
 import com.app.bot.Bot;
 import com.app.commands.Command;
-import com.app.settings.Emojis;
+import com.app.commands.parser.WeatherMessageParser;
 import com.app.settings.Settings;
 import com.github.prominence.openweathermap.api.enums.Language;
 import com.github.prominence.openweathermap.api.enums.UnitSystem;
-import com.github.prominence.openweathermap.api.model.weather.Rain;
-import com.github.prominence.openweathermap.api.model.weather.Snow;
 import com.github.prominence.openweathermap.api.model.weather.Weather;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
-
-import java.time.LocalTime;
 
 public class WeatherNowCommand extends Command {
 
@@ -39,96 +35,18 @@ public class WeatherNowCommand extends Command {
     }
 
     private String makeWeatherNowString(Weather weather) {
-        return getCityLine(weather) +
+        return WeatherMessageParser.getCityLine(weather) +
                 "\n" +
-                getTemperatureLineInCelsius(weather) +
+                WeatherMessageParser.getTemperatureNowLineInCelsius(weather) +
                 "\n" +
-                getCloudinessLine(weather) +
+                WeatherMessageParser.getCloudinessLine(weather) +
                 "\n" +
-                getRainAndSnowLines(weather) +
+                WeatherMessageParser.getRainAndSnowLines(weather) +
                 "\n" +
-                getWindLine(weather) +
+                WeatherMessageParser.getWindLine(weather) +
                 "\n" +
-                getHumidityLine(weather);
-    }
-
-    private String getCityLine(Weather weather) {
-        String city = weather.getLocation().getName();
-        byte hour = (byte) LocalTime.now().getHour();
-        city = (hour >= 7 && hour <= 12) ? Emojis.MORNING_CITY + " " + city : city;
-        city = (hour >= 13 && hour <= 18) ? Emojis.DAY_CITY + " " + city : city;
-        city = (hour >= 19) ? Emojis.EVENING_CITY + " " + city : city;
-        city = (hour <= 6) ? Emojis.NIGHT_CITY + " " + city : city;
-        return city + "\n";
-    }
-
-    private String getTemperatureLineInCelsius(Weather weather) {
-        byte temp = (byte) Math.round(weather.getTemperature().getValue());
-        String temperature = "";
-        temperature = (temp >= 30) ? Emojis.THIRTY_AND_HIGHER + " " + temp : temperature;
-        temperature = (temp >= 20 && temp < 30) ? Emojis.TWENTY_AND_TWENTY_NINE + " " + temp : temperature;
-        temperature = (temp >= 10 && temp < 20) ? Emojis.TEN_AND_NINETEEN + " " + temp : temperature;
-        temperature = (temp >= 0 && temp < 10) ? Emojis.ZERO_AND_NINE + " " + temp : temperature;
-        temperature = (temp >= -15 && temp < 0) ? Emojis.SUB_FIFTEEN_AND_ONE + " " + temp : temperature;
-        temperature = (temp >= -20 && temp < -15) ? Emojis.SUB_TWENTY_AND_SIXTEEN + " " + temp : temperature;
-        temperature = (temp < -20) ? Emojis.SUB_TWENTY_ONE + " " + temp : temperature;
-        return temperature + " °C\n";
-    }
-
-    private String getHumidityLine(Weather weather) {
-        int humidity = weather.getHumidity().getValue();
-        return Emojis.HUMIDITY + " " + humidity + "%";
-    }
-
-    private String getCloudinessLine(Weather weather) {
-        byte cloud = weather.getClouds().getValue();
-        String cloudiness = "";
-        cloudiness = (cloud < 20) ? Emojis.CLEAR + " Ясно" : cloudiness;
-        cloudiness = (cloud >= 20 && cloud < 40) ? Emojis.PARTLY_CLOUDY_1 + " Малооблачно" : cloudiness;
-        cloudiness = (cloud >= 40 && cloud < 60) ? Emojis.PARTLY_CLOUDY_2 + " Переменная облачность" : cloudiness;
-        cloudiness = (cloud >= 60 && cloud < 80) ? Emojis.PARTLY_CLOUDY_3 + " Облачно с прояснениями" : cloudiness;
-        cloudiness = (cloud <= 100) ? Emojis.CLOUDY + " Облачно/Пасмурно" : cloudiness;
-        return cloudiness + "\n";
-    }
-
-    private String getRainAndSnowLines(Weather weather) {
-        Rain rainTemp = weather.getRain();
-        Snow snowTemp = weather.getSnow();
-        if (rainTemp != null && snowTemp != null) {
-            // TODO: Доделать дождь + снег
-        } else if (rainTemp != null) {
-            double rain = rainTemp.getOneHourLevel();
-            String rainLine = "";
-            rainLine += (rain < 4.0) ? Emojis.SMALL_RAIN_1_2 + " Небольшой дождь" : rainLine;
-            rainLine += (rain >= 4.0 && rain < 15.0) ? Emojis.SMALL_RAIN_1_2 + " Дождь" : rainLine;
-            rainLine += (rain >= 15.0 && rain < 50.0) ? Emojis.RAIN_1_2 + " Сильный дождь" : rainLine;
-            rainLine += (rain >= 50.0) ? Emojis.RAIN_1_2 + " Очень сильный дождь" : rainLine;
-            return rainLine + "\n";
-        } else if (snowTemp != null) {
-            double snow = snowTemp.getOneHourLevel();
-            String snowLine = "";
-            snowLine += (snow < 4.0) ? Emojis.SNOW_1_2_3_4 + " Небольшой снег" : snowLine;
-            snowLine += (snow >= 4.0 && snow < 15.0) ? Emojis.SNOW_1_2_3_4 + " Снег" : snowLine;
-            snowLine += (snow >= 15.0 && snow < 50.0) ? Emojis.SNOW_1_2_3_4 + " Сильный снег" : snowLine;
-            snowLine += (snow >= 50.0) ? Emojis.SNOW_1_2_3_4 + " Очень сильный снег" : snowLine;
-            return snowLine + "\n";
-        }
-        return Emojis.NO_PRECIPITATION + " Без осадков!\n";
-    }
-
-    private String getWindLine(Weather weather) {
-        try {
-            byte wind = (byte) Math.round(weather.getWind().getSpeed());
-            String windLine = "";
-            windLine = (wind <= 5) ? Emojis.WIND_1 + " Слабый ветер" : windLine;
-            windLine = (wind > 5 && wind <= 14) ? Emojis.WIND_2 + " Умеренный ветер" : windLine;
-            windLine = (wind > 14 & wind <= 24) ? Emojis.WIND_3 + " Сильный ветер" : windLine;
-            windLine = (wind > 24 && wind <= 32) ? Emojis.WIND_3 + " Очень сильный ветер" : windLine;
-            windLine = (wind > 32) ? Emojis.WIND_4 + " Ураганный ветер" : windLine;
-            return windLine + "\n";
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-        return "Ветер умер";
+                WeatherMessageParser.getHPaPressureLine(weather) +
+                "\n" +
+                WeatherMessageParser.getHumidityLine(weather);
     }
 }
--- src/main/java/com/app/settings/Emojis.java
+++ src/main/java/com/app/settings/Emojis.java
@@ -1,34 +1,5 @@
 package com.app.settings;
 
-<<<<<<< Updated upstream
-public abstract class Emojis {
-    public static String MORNING_CITY = "\ud83c\udf07";
-    public static String DAY_CITY = "\uD83C\uDFD9";
-    public static String EVENING_CITY = "\ud83c\udf06";
-    public static String NIGHT_CITY = "\ud83c\udf03";
-    public static String THIRTY_AND_HIGHER = "\ud83d\udd25";
-    public static String TWENTY_AND_TWENTY_NINE = "\ud83e\udd75";
-    public static String TEN_AND_NINETEEN = "\ud83d\ude0e";
-    public static String ZERO_AND_NINE = "\ud83e\udde5";
-    public static String SUB_FIFTEEN_AND_ONE = "\u2744\ufe0f";
-    public static String SUB_TWENTY_AND_SIXTEEN = "\ud83e\udde3";
-    public static String SUB_TWENTY_ONE = "\ud83e\udd76";
-    public static String HUMIDITY = "\ud83d\udca7";
-    public static String SNOW_1_2_3_4 = "\ud83c\udf28";
-    public static String CLEAR = "\u2600\ufe0f";
-    public static String PARTLY_CLOUDY_1 = "\ud83c\udf24";
-    public static String PARTLY_CLOUDY_2 = "\u26c5\ufe0f";
-    public static String PARTLY_CLOUDY_3 = "\ud83c\udf25";
-    public static String CLOUDY = "\u2601\ufe0f";
-    public static String SMALL_RAIN_1_2 = "\ud83c\udf26";
-    public static String RAIN_1_2 = "\ud83c\udf26";
-    public static String NO_PRECIPITATION = "\ud83c\udf08";
-    public static String WIND_1 = "\ud83c\udf43";
-    public static String WIND_2 = "\ud83d\udca8";
-    public static String WIND_3 = "\ud83c\udf2c";
-    public static String WIND_4 = "\ud83c\udf2a";
-    public static String PRESSURE = "\ud83c\udf21";
-=======
 public enum Emojis {
     MORNING_CITY("\ud83c\udf07"),
     DAY_CITY("\uD83C\uDFD9"),
@@ -60,11 +31,9 @@ public enum Emojis {
     WIND_4("\ud83c\udf2a"),
     PRESSURE("\ud83c\udf21");
 
-
     public final String hexCode;
 
     private Emojis(String hexCode) {
         this.hexCode = hexCode;
     }
->>>>>>> Stashed changes
 }
